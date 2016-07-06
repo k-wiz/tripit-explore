0d5fed93db250d4136a8579347726a7a7b406c01
kelli <kwisuri@gmail.com>
2016-07-06 15:01:19 -0700

    Created db; set up SQLAlchemy. App workimg completely. Next: parse XML.

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from flask import Flask,redirect, request
-app = Flask(__name__)
+from flask_debugtoolbar import DebugToolbarExtension
+from bs4 import BeautifulSoup
+from jinja2 import StrictUndefined
+from model import connect_to_db, db
 import tripit
 import xmltodict
 import os
+
+app = Flask(__name__)
+app.secret_key = "efi8y9jduyo2"
+app.jinja_env.undefined = StrictUndefined
+
 
 
 
@@ -21,9 +29,12 @@ oauth_token_secret = token_data["oauth_token_secret"]
 
 
 
+
 @app.route('/')
 def hello():
     return redirect('https://www.tripit.com/oauth/authorize?oauth_token=%s&oauth_callback=http://127.0.0.1:5000/callback/?oauth_token_secret=%s&' % (oauth_token, oauth_token_secret))
+
+
 
 @app.route('/callback/')
 def process_token():
@@ -42,14 +53,23 @@ def process_token():
     oauth_credential = tripit.OAuthConsumerCredential(consumer_key, consumer_secret, authorized_oauth_token, authorized_oauth_token_secret)
     t = tripit.TripIt(oauth_credential, api_url='https://api.tripit.com')
 
-    '''
-    t.get_profile()
+    # '''
+    # t.get_profile()
 
-    profile = xmltodict.parse(t.response)
-    user_id = profile['Response']['Profile']['ProfileEmailAddresses']['ProfileEmailAddresses']
-    '''
+    # profile = xmltodict.parse(t.response)
+    # user_id = profile['Response']['Profile']['ProfileEmailAddresses']['ProfileEmailAddresses']
+    # '''
     t.list_trip()
+    print t.list_trip()
+    print t.response
     return "%s" % t.response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+
+    app.debug = True
+    connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
     app.run()
